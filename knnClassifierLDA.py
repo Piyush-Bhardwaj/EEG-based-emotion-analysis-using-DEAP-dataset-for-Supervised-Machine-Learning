4b8ff31dc8787b06e5b2a14495b8ad4337ff53de
Piyush Bhardwaj <46351251+Piyush-Bhardwaj@users.noreply.github.com>
2019-01-10 22:48:53 +0530

    Update knnClassifierLDA.py

--- knnClassifierLDA.py
+++ knnClassifierLDA.py
@@ -3,8 +3,11 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
+import warnings
+
 
 def knn_classifier_lda():
+    warnings.filterwarnings("ignore")
     file_x = 'data/features_raw.dat'
     file_y = 'data/label_class_0.dat'
     
