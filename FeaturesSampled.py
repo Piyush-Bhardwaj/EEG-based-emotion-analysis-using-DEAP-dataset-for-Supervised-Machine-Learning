1e6f2d2e7e5ed85c4bd48d2b3d8c171230703428
Piyush Bhardwaj <46351251+Piyush-Bhardwaj@users.noreply.github.com>
2019-01-10 20:43:19 +0530

    Add files via upload

---
+++ FeaturesSampled.py
@@ -0,0 +1,29 @@
+import pickle
+
+nLabel, nTrial, nUser, nChannel, nTime  = 4, 40, 32, 40, 8064  #10080/80=126*40=5040 features
+no_of_users=3
+def sampleFeatures():
+    print("Program started"+"\n")
+    fout_data = open('data/features_sampled.dat','w')
+    for i in range(no_of_users):   #nUser  #4, 40, 32, 40, 8064
+        if(i%1 == 0):
+            if i < 10:			
+                name = '%0*d' % (2,i+1)
+            else:
+                name = i+1		
+            fname = "data/s"+str(name)+".dat"
+            x = pickle.load(open(fname, 'rb'), encoding='latin1')		
+            print(fname)		
+            for tr in range(nTrial):			
+                if(tr%1 == 0):				
+                    for dat in range(nTime):					
+                        if(dat%32 == 0 ):						
+                            for ch in range(nChannel):							
+                                fout_data.write(str(ch+1) + " ");							
+                                fout_data.write(str(x['data'][tr][ch][dat]) + " ");			
+                fout_data.write("\n");
+    fout_data.close()
+    print('Completed')
+
+if __name__ == '__main__':
+    sampleFeatures()
---
+++ data_visual.py
@@ -0,0 +1,44 @@
+def data_visual():
+    import pandas as pd
+    import seaborn as sns
+    from pandas.plotting import scatter_matrix
+    import matplotlib.pyplot as plt
+    
+    features = pd.read_csv('features.csv') 
+    labels = pd.read_csv('labels.csv')
+
+    names = ['valence', 'arousal', 'dominance', 'liking']
+    dataset =features.transpose().reindex()
+
+
+# Summary of dataset
+    print('Summary of dataset\n')
+# shape
+    print('1. Shape is:')
+    print(dataset.shape)
+# head
+    print('\n2. First 8 rows are as:')
+    print(dataset.head(8))
+# descriptions
+    print('\n3. Statistical description:')
+    print(dataset.describe())
+    types = dataset.dtypes
+    print('\n4. Data Types:')
+    print(types)
+
+# Data visualisation
+# box and whisker plots
+    for i in range(5):
+        sns.set_style('whitegrid')
+        sns.boxplot(dataset[i])
+        plt.title('Magnitude versus ' + str(i+1) + ' channel')
+        plt.show()
+    
+    import seaborn as sns
+    from matplotlib import pyplot as plt
+    for i in range(5):
+        sns.distplot(dataset[i])
+        plt.title('Magnitude versus ' + str(i+1) + ' channel')
+        plt.show()
+if __name__ == '__main__':
+   data_visual()
